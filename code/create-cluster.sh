a569264bcd030d6b1760e1e9c2bf42a973d66ab2
kuma <kuma@kp.local>
2015-10-27 02:24:37 +0900

    first draft

---
+++ code/create-cluster.sh
@@ -0,0 +1,1 @@
+gcloud container --project "(project-name)" clusters create "cluster-1" --zone "asia-east1-a" --machine-type "n1-standard-1" --network "cluster-1"
---
+++ code/create-repcon.sh
@@ -0,0 +1,1 @@
+kubectl create -f frontend-replication-controller.yaml
